369e2fd31797e1ee530a83ea4ad77c7091c323fb
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-18 15:36:41 +0300

    wip: baraka smiles

--- resources/views/templates/hero/_text_area.blade.php
+++ resources/views/templates/hero/_text_area.blade.php
@@ -1,4 +1,4 @@
-<div class="">
+<div class="py-4">
 
     <div class=" @if($hasBorder) rounded-md  bg-gray-100 border-b-4 border-primary-600 border-b-primary-600 @endif">
         {{ str($html)->trim(' ')->toHtmlString() }}
