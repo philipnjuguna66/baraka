d9f5fdfcf349725b205bd4edac64ab77bd21847e
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-18 15:02:54 +0300

    wip: baraka smiles

--- app/Models/ServiceSection.php
+++ app/Models/ServiceSection.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Utils\Enums\SectionEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ServiceSection extends Model
 {
     use HasFactory;
+    protected $casts = [
+        'type' => SectionEnum::class,
+        'extra' => 'json',
+    ];
 }
