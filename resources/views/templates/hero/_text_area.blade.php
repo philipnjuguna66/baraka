3f0d112e4b7d45cee4f93e3b89a2fcd200cf736e
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-18 15:35:41 +0300

    wip: baraka smiles

--- resources/views/templates/hero/_text_area.blade.php
+++ resources/views/templates/hero/_text_area.blade.php
@@ -1,6 +1,6 @@
 <div class="">
 
-    <div class="prose-md  md:text-justify @if($hasBorder) shadow-md rounded-md px-4 mt-5 bg-gray-100 py-8 border-b-4 border-primary-600 border-b-primary-600 @endif">
+    <div class=" @if($hasBorder) rounded-md  bg-gray-100 py-8 border-b-4 border-primary-600 border-b-primary-600 @endif">
         {{ str($html)->trim(' ')->toHtmlString() }}
     </div>
 
