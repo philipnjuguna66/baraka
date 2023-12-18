40124abad4582c7d729a04d4cd3ae880ce82ac1b
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-18 11:28:19 +0300

    wip: baraka smiles

--- resources/views/templates/hero/page_builder.blade.php
+++ resources/views/templates/hero/page_builder.blade.php
@@ -8,7 +8,7 @@ bg-opacity-90 py-12 bg-gray-950 bg-cover bg-center bg-norepeat text-white
          style=" background-image: url('{{ url(Storage::url($section->extra['bg_image'])) }}');
          background-color: #4a5568;
      background-position: center center; background-size: cover; background-repeat: no-repeat "
-     @endif
+    @endif
 
 >
     <div class="mx-auto w-4/5 max-w-7xl  lg:px-8">
@@ -25,26 +25,24 @@ bg-opacity-90 py-12 bg-gray-950 bg-cover bg-center bg-norepeat text-white
                     @foreach($columns as $column)
 
                             <?php
-                            $html = match ($column['type'])
-                            {
+                            $html = match ($column['type']) {
                                 "header" => view('templates.hero._header', ['heading' => $column['data']['heading'], "subheading" => $column['data']['subheading']])->render(),
                                 "video" => view('templates.embeded._video_iframe', ["autoplay" => 0, 'videoUri' => $column['data']['video_path']])->render(),
-                                "image" => view('templates.hero._image', ['image' => $column['data']['image'],'title' => $column['data']['title'],'section' => $section])->render(),
+                                "image" => view('templates.hero._image', ['image' => $column['data']['image'], 'title' => $column['data']['title'], 'section' => $section])->render(),
                                 "booking_form" => view('templates.hero._site')->render(),
                                 "text_area" => view('templates.hero._text_area', ['html' => $column['data']['body'], 'hasBorder' => $column['data']['has_border_color'] ?? false])->render(),
-                                "slider" => view('templates.hero._slider', ['sliders' => $column['data']['body'],'page' => $page])->render(),
+                                "slider" => view('templates.hero._slider', ['sliders' => $column['data']['body'], 'page' => $page])->render(),
                                 "masonary_block" => view('templates.hero.masionary', ['masonrySections' => $column['data'], 'page' => $page])->render(),
-                                "header_section" => view("templates.cta.heading",['section' => $column]),
-                                "buttons" => view("templates.hero._buttons",['section' => $column['data']]),
+                                "header_section" => view("templates.cta.heading", ['section' => $column]),
+                                "buttons" => view("templates.hero._buttons", ['section' => $column['data']]),
                                 "default" => null,
                             };
                             ?>
-                    <div class="mx-auto px-8">
-                        {{ str($html)->toHtmlString() }}
+                        <div class="mx-auto px-8">
+                            {{ str($html)->toHtmlString() }}
 
 
-                    </div>
-
+                        </div>
 
                     @endforeach
                 </div>
@@ -52,15 +50,21 @@ bg-opacity-90 py-12 bg-gray-950 bg-cover bg-center bg-norepeat text-white
             @endforeach
 
             @if( $section->extra['url']  ?? false)
-                    <div class="mx-auto max-w-5xl w-4/5">
-                        <a
-                            href="{{ $section->extra['url'] }}"
-                            class="button"
-                            wire:navigate
-                        >
-                            {{ $section->extra['label'] }}
-                        </a>
+                <div class=" ">
+                    <div class="px-6 py-2 sm:px-6 sm:py-1 lg:px-8">
+                        <div class="md:mx-auto max-w-2xl text-center">
+                            <div class="mt-10 flex items-center justify-center gap-x-6">
+                                <a
+                                    href="{{ $section->extra['url'] }}"
+                                    class="button"
+                                    wire:navigate
+                                >
+                                    {{ $section->extra['label'] }}
+                                </a>
+                            </div>
+                        </div>
                     </div>
+                </div>
             @endif
 
 
