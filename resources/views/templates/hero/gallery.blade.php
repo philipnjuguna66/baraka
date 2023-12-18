85ad701c2ee5d4672c88d6b7140d5707b7cf2edc
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-18 12:28:47 +0300

    wip: baraka smiles

--- resources/views/templates/hero/gallery.blade.php
+++ resources/views/templates/hero/gallery.blade.php
@@ -8,8 +8,10 @@
                 <img class="h-auto max-w-full rounded-lg object-cover object-center"
                      src="{{  \Illuminate\Support\Facades\Storage::url($image['image'])}}"
                      alt="{{ $page->meta_title }}">
+                <div class="mx-auto w-2/5 max-w-2xl flex justify-center items-center text-center">
 
-                <span class="text-center">{{ isset($image['image_name']) ? $image['image_name'] : ""}}</span>
+                    <span class="text-center font-normal text-lg  py-2">{{ isset($image['image_name']) ? $image['image_name'] : ""}}</span>
+                </div>
 
 
             </div>
